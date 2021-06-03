8d0c4f9550947a33332e1b2be9aafbab4ac580c7
dmorais.eng.comp <dmorais.eng.comp@gmail.com>
2021-06-03 20:47:40 -0300

    Refatorando Projeto. utilizando ORM Sequelize. Utilizando MySQL.

--- route/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const loginController = require('../controllers/login/index')
-const userController = require('../controllers/user/user')
-router.use('/login', loginController)
-router.use('/user', userController)
-
-module.exports = router;
